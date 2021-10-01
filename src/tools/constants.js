04b19815bd953e5acb42b90983fe887078cb0e73
SMoni <steffen.monarth@ingenieur.io>
2021-10-01 21:51:00 +0200

    Adding diminished and augmented

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -4,25 +4,27 @@ const scales = {
 }
 
 const modes = {
-    major:            [ 2, 2, 1, 2, 2, 2, 1 ], // ionian
+    major:            [ 2, 2, 1, 2, 2, 2 ], // ionian
+    minor:            [ 2, 1, 2, 2, 1, 2 ], // aeolian
+    ionian:           [ 2, 2, 1, 2, 2, 2 ],
+    dorian:           [ 2, 1, 2, 2, 2, 1 ],
+    phrygian:         [ 1, 2, 2, 2, 1, 2 ],
+    lydian:           [ 2, 2, 2, 1, 2, 2 ],
+    mixolydian:       [ 2, 2, 1, 2, 2, 1 ],
+    aeolian:          [ 2, 1, 2, 2, 1, 2 ],
+    locrian:          [ 1, 2, 2, 1, 2, 2 ],
     majorPentatonic:  [ 2, 2, 3, 2 ], 
-    minor:            [ 2, 1, 2, 2, 1, 2, 2 ], // aeolian
     minorPentatonic:  [ 3, 2, 2, 3 ], 
-    minorHarmonic:    [ 2, 1, 2, 2, 1, 3, 1 ],
     bluesHexatonic:   [ 3, 2, 1, 1, 3 ], 
-    ionian:           [ 2, 2, 1, 2, 2, 2, 1 ],
-    dorian:           [ 2, 1, 2, 2, 2, 1, 2 ],
-    phrygian:         [ 1, 2, 2, 2, 1, 2, 2 ],
-    phrygianDominant: [ 1, 3, 1, 2, 1, 2, 2 ],    
-    lydian:           [ 2, 2, 2, 1, 2, 2, 1 ],
-    mixolydian:       [ 2, 2, 1, 2, 2, 1, 2 ],
-    aeolian:          [ 2, 1, 2, 2, 1, 2, 2 ],
-    locrian:          [ 1, 2, 2, 1, 2, 2, 2 ],
+    minorHarmonic:    [ 2, 1, 2, 2, 1, 3 ],
+    phrygianDominant: [ 1, 3, 1, 2, 1, 2 ],    
 }
 
 const chords = {
     major:        [ 4, 3 ],
     minor:        [ 3, 4 ],
+    augmented:    [ 4, 4 ],
+    diminished:   [ 3, 3 ],
     majorSeventh: [ 4, 3, 3 ],
     minorSeventh: [ 3, 4, 3 ],
     sus2:         [ 2, 5 ],
